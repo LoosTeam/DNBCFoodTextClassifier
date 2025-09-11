175d4848fcf5da16d0619c083b92e3b65388100d
Siddhi Jain <siddhiyjain@gmail.com>
2025-09-11 12:24:21 +0200

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -28,7 +28,7 @@ golem::fill_desc(
     email = "AUTHOR@MAIL.COM", # Your email
     role = c("aut", "cre") # Your role (here author/creator)
   ),
-  repo_url = NULL, # The URL of the GitHub repo (optional),
+  repo_url = "https://github.com/LoosTeam/DNBCFoodTextClassifier", # The URL of the GitHub repo (optional),
   pkg_version = "0.0.0.9000", # The version of the package containing the app
   set_options = TRUE # Set the global golem options
 )
